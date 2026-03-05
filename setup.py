64a21c4f0da7a6c2040543574b44b236a1a31b51
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-03-05 18:41:28 +0900

    setup.py 만들기

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lcl',
+    version='0.0.1',
+    description='LCL C-ymbolic (C - symbolic) Link-ing (link introduce linking)',
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    url='https://github.com/FarAway6834/LCL',
+    packages=find_packages(exclude=[]),
+    install_requires=['PyPInclude'],
+    keywords=['python', 'PyPI', 'PyPInclude', "LCL", "lcl", "Cymbolic", "C", "c", "CPP", "cpp", "symbolic", "link", "linking", "introduce"],
+    python_requires='>=3.6',
+    package_data={},
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+)
