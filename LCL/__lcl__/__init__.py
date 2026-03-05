512cc4fd44b7ec9d3d730f13962b3bb4b6565ede
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-03-05 18:24:56 +0900

    __init__.py 업데이트

--- LCL/__lcl__/__init__.py
+++ LCL/__lcl__/__init__.py
@@ -1,1 +1,9 @@
+from os import symlink as __sym_link__
+from os.path import dirname as __get_dir__
+from PyPInclude import lib as __lib__
+from time import time as __time__
+from random import random as __random__
+
 __all__ = []
+
+__lib_pw__ = hash(str(__time__() + __random__()))
